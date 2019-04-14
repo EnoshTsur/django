668fdc2db30ab877a1f196658a1d1671c9a59dd8
Yahalom <yahalom@Nesstr.com>
2019-04-14 21:32:15 +0300

    Django prject

--- venv/sample_site/books/models.py
+++ venv/sample_site/books/models.py
@@ -2,14 +2,27 @@ from django.db import models
 
 # Create your models here.
 class Author(models.Model):
+    
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return f'Author name: {self.name}'
 
+class Customer(models.Model):
+
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'Name: {self.name}, Address: {self.address}'
 
 class Book(models.Model):
+    
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    customers = models.ManyToManyField(Customer)
     name = models.CharField(max_length=250)
     genre = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'Name: {self.name} , Genre: {self.genre}'
 
