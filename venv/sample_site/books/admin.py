7ce259ee55785324bc3e0449520e502af99f6efe
Enosh_Tsur <enoshh12@gmail.com>
2019-05-05 16:56:31 +0300

    admin super user

--- venv/sample_site/books/admin.py
+++ venv/sample_site/books/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Author, Book, Customer
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Customer)
